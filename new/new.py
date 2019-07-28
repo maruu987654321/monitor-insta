812767f46cb81627b69ccb6f7e17285597693aa0
ruta777 <evgfed777@gmail.com>
2019-07-29 00:41:42 +0300

    add

---
+++ new/new.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+def print_hello():
+    a = 'print_hello_string'
+    b = 'print_hello_string_2'
+    c = b
+    print(a)
+    print(b)
+    return c
+
+
+
+if __name__ == "__main__":
+    print_hello()
+    print('else print else')
